be9697d94f61ed27b203bb23accbf1ff991ec71f
seveniu <prime3721@gmail.com>
2018-01-04 16:26:53 +0800

    更新 mysql 修复时区问题

--- mysql/build.sh
+++ mysql/build.sh
@@ -6,6 +6,6 @@ then
     script_dir="$current_dir"
 fi
 echo $script_dir
-mysqlVersion=5.7.17
+mysqlVersion=5.7.20
 docker run --name mysql-${mysqlVersion} -v ${script_dir}/conf:/etc/mysql/conf.d -e TZ='Asia/Shanghai' -e MYSQL_ROOT_PASSWORD=passw0rd -p 3306:3306 -d mysql:${mysqlVersion}
 
